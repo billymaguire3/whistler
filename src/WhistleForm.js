7e4ce9bdb19fe523bb05f114af38e2b49052716d
Billy Maguire <billymaguire3@gmail.com>
2021-02-03 14:26:41 -0500

    Test is passing to check the functionality of textarea's onChange method
    Here we add a constructor with state of 'value' to our class component WhistleForm.
    Then, add the initial value of an empty string to our textarea tag.
    Then, add an onChange method prop to our textarea tag, which invokes the
    handleChange method of our component, which sets the state of our value
    key to be updated with whatever text is inputted into the textarea by
    the user.

--- src/WhistleForm.js
+++ src/WhistleForm.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 class WhistleForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return (
       <div>
         <form className="whistle-form">
           <label>
             New Whistle Here
-            <textarea />
+            <textarea
+              name="value"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
           </label>
         </form>
       </div>

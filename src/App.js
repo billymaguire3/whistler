2e8f5894208d43bc4d286a6f5835135c44669eb2
Billy Maguire <billymaguire3@gmail.com>
2021-02-03 14:54:54 -0500

    Failing test now gives expected error
    The test expects Enzyme's invoke method to have the arguments of a prop
    whose value is a function. Make the test pass next please

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import WhistleForm from './WhistleForm';
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="App">
